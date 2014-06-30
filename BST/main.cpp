4e58c0b04b7c225adcb1b974f38344c94fe8f1c5
adrianma <adrian.mascarenhas@microsoft.com>
2014-06-30 00:17:52 -0700

    created BST class and IsBST

--- BST/main.cpp
+++ BST/main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+//BST cannot have duplicate nodes.
+
 
 /*
 struct vs class
@@ -11,18 +13,28 @@ using private/public modifiers.
 
 structs are typically used for POD (plain old data) types.
 
+*/
 
-template <typename T>
+template<typename T>
 struct NODE
 {
-	T item;
-	NODE * left, * right;
+	T Value;
+	NODE * Left, *Right;
+
+	NODE(T val) : 
+		Value(val), 
+		Left(nullptr), 
+		Right(nullptr) 
+	{}
 };
 
-*/
+#define TRAVERSE_PREORDER 0
+#define TRAVERSE_INORDER 1
+#define TRAVERSE_POSTORDER 2
 
 
 /*
+//Build a BST class with templates
 
 Search
 Insert
@@ -33,109 +45,168 @@ IsBST
 IsBalanced
 GetDepth
 
-
 */
 
 
-/*
-Build a BST class with templates
 
 template<typename T>   // you can use <typename T> or <class T> , they both mean same. Typename is more clear since it can mean primitive types as well as classes.
-struct  BST
+class  BST
 {
-	NODE * Root;
+private:
+	NODE<T> * Root;
 
-	BST();
-	void Insert(NODE * node);
-	void Insert(T items []);
-	void Display();
+	//When defining template methods, you have to define them inline.
+	NODE<T>* FindTreeItemImpl(NODE<T>* rootNode, T item)
+	{
+		if (rootNode == nullptr) return rootNode;
+
+		if (rootNode->Value == item)
+			return rootNode;
+
+		if (item < rootNode->Value)
+			return FindTreeItemImpl(rootNode->Left, item);
+		else
+			return FindTreeItemImpl(rootNode->Right, item);
+	}
+
+	void InsertImpl(_Inout_ NODE<T>** node, _In_ T item)   //could use NODE<T>*& instead of NODE<T>** ie. pointer by ref instead of pointer to pointer. No difference, just depends on preference.
+	{
+		if (*node == nullptr)
+		{
+			*node = new NODE<T>(item);
+			return;
+		}
+
+		if (item < (*node)->Value)
+			InsertImpl(&(*node)->Left, item);
+		else if (item >(*node)->Value)
+			InsertImpl(&(*node)->Right, item);
+		//ignore duplicates.
+	}
+
+	//By default traverse postorder
+	void DisplayTreeImpl(_In_ NODE<T>* node, _In_ int indent = 0, _In_ char* LR = "") //can specify default values for optional parameters.
+	{
+		
+		//do post-order traversal to show the tree from left to right (instead of top down) and right (top) to left (bottom). 
+		if (node == nullptr) return;
+		DisplayTreeImpl(node->Right, indent + 6, "R:");
+
+		std::cout << setw(indent) << " ";   //Specifies the width of the display field for the next element in the stream.
+		cout << LR << node->Value << endl;
+
+		DisplayTreeImpl(node->Left, indent + 6, "L:");
+
+
+	}
+
+	void DeleteTreeImpl(_In_ NODE<T>* node)
+	{
+		DeleteTreeImpl(node->Left);
+		DeleteTreeImpl(node->Right);
+		delete(node);
+	}
+
+
+	bool IsBSTImpl(_In_ NODE<T> * node, T minValue, T maxValue)
+	{
+		if (node == nullptr) return true;
+		if (node->Value > maxValue || node->Value < minValue)     //if node violates limits passed down then it is not BST
+		{
+			cout << "Not a BST: Violating node :" << node->Value << "\n";
+			return false;
+		}
+
+		return IsBSTImpl(node->Left, minValue, node->Value)   // the left subtree has a max limit
+			&& IsBSTImpl(node->Right, node->Value, maxValue);   //right subtree has min limit
+	}
+	
+
+public:
+	BST() : Root(nullptr) {};
+
+	//Insert 
+	void Insert(_In_ T item)
+	{
+		InsertImpl(&Root, item);
+	}
+
+	// have to pass in count since when we pass in int[] , it gets converted to int * (and it loses array count info) and _countof does not like int *. Also sizeof loses the information of array count.
+	void Insert(_In_ T items[], int countOfItems)
+	{
+		for (int i = 0; i < countOfItems; i++)
+		{
+			Insert(items[i]);
+		}
+	}
+
+
+	void DeleteTree()
+	{
+		DeleteTreeImpl(this->Root);
+	}
+
+	//void Delete(T item);
+	bool IsBST()
+	{
+		return IsBSTImpl(this->Root, INT_MIN, INT_MAX);
+	}
+
+	NODE<T>* FindTreeItem(T item)
+	{
+		return FindTreeItemImpl(this->Root, item);
+	}
+
+
+	void DisplayTree()
+	{
+		DisplayTreeImpl(this->Root);
+	}
+
 
 };
 
-*/
 
 
-template<typename T>
-struct NODE
-{
-	T Value;
-	NODE * Left = nullptr, *Right = nullptr;
-
-	NODE(int val) : Value(val) {}
-};
-
-
-template<typename T>
-void Insert(_Inout_ NODE<T>** Root, _In_ NODE<T> * item)
-{
-	if (*Root == nullptr)
-	{
-		*Root = item;
-		return;
-	}
-
-	if (item->Value < (*Root)->Value)
-		Insert(&(*Root)->Left, item);
-	else
-		Insert(&(*Root)->Right, item);
-}
-
-// have to pass in count since when we pass in int[] , it gets converted to int * (and it loses array count info) and _countof does not like int *. Also sizeof loses the information of array count.
-template<typename T>
-void Insert(_Inout_ NODE<T> ** Root, _In_ T items[], int countOfItems)
-{
-
-	for (int i = 0; i < countOfItems; i++)
-	{
-		Insert(Root, new NODE<T>(items[i]));
-	}
-
-}
-
-template<typename T>
-void DisplayTree(NODE<T>* Root, int indent = 0, char* LR = "")
-{
-	//do post-order traversal to show the tree from left to right (instead of top down) and right (top) to left (bottom). 
-	if (Root == nullptr) return;
-	DisplayTree(Root->Right, indent + 6, "R:");
-	std::cout << setw(indent) << " ";   //Specifies the width of the display field for the next element in the stream.
-	cout << LR << Root->Value << endl;
-	DisplayTree(Root->Left, indent + 6, "L:");
-}
-
-template<typename T>
-void DeleteTree(NODE<T> * Root)
-{
-	if (Root == nullptr) return;
-	DeleteTree(Root->Left);
-	DeleteTree(Root->Right);
-	delete(Root);
-}
 
 int main()
 {
-	NODE<int>* RootInt = nullptr;
+	BST<int> * BSTInt = new BST<int>();
 
 	int items[7] = { 4, 2, 6, 1, 3, 5, 7 };
-	Insert<int>(&RootInt, items, _countof(items)); // have to pass in count since when we pass in int[] , it gets converted to int * and _countof does not like int *
-	DisplayTree<int>(RootInt);
-	printf("\n\n\n\n");
+	BSTInt->Insert(items, _countof(items)); // have to pass in count since when we pass in int[] , it gets converted to int * and _countof does not like int *
+	BSTInt->DisplayTree();
+	printf("\nThe tree is a BST : %s\n\n", BSTInt->IsBST()? "True" : "False");
 
-	int items2[4] = { 18, 5, 13, 17 };
-	Insert<int>(&RootInt, items2, _countof(items2));
-	DisplayTree<int>(RootInt);
-	printf("\n\n\n\n");
 
-	NODE<char> * RootChar = nullptr;
+	int items2[5] = { 18, 5, 13, 17, 6 };
+	BSTInt->Insert(items2, _countof(items2));
+	BSTInt->DisplayTree();
+	printf("\nThe tree is a BST : %s\n\n", BSTInt->IsBST() ? "True" : "False");
+
+	/* The auto keyword is a declaration specifier. However, the C++ standard defines an original and a revised meaning for this keyword.
+	Before Visual C++ 2010, the auto keyword declares a variable in the automatic storage class; that is, a variable that has a local lifetime.
+	Starting with Visual C++ 2010, the auto keyword declares a variable whose type is deduced from the initialization expression in its declaration. The /Zc:auto[-] compiler option controls the meaning of the auto keyword.
+	*/
+	auto t1 = BSTInt->FindTreeItem(3);  //auto is a nice way of 
+	auto t2 = BSTInt->FindTreeItem(5);
+	t1->Value = 5; t2->Value = 3;
+	BSTInt->DisplayTree();
+	printf("\nThe tree is a BST : %s\n\n", BSTInt->IsBST() ? "True" : "False");
+
+
+
+	BST<char>* BSTChar = new BST<char>();
 
 	char itemsChar[7] = {'F', 'D', 'J', 'A', 'K', 'E', 'C'};
-	Insert<char>(&RootChar, itemsChar, _countof(itemsChar)); // have to pass in count since when we pass in int[] , it gets converted to int * and _countof does not like int *
-	DisplayTree<char>(RootChar);
-	printf("\n\n\n\n");
+	BSTChar->Insert(itemsChar, _countof(itemsChar)); 
+	BSTChar->DisplayTree();
+	//printf("\nThe tree is a BST : %s\n\n", BSTChar->IsBST() ? "True" : "False");
+
 
 
 
 	int n;
-	cin >> n;
+	std::cin >> n;
 
 }
